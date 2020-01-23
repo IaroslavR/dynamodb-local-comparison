4d404ddbb9fb1516e40e057718c310ab46ab2ba8
IaroslavR <iarruss@ya.ru>
2020-01-23 22:37:10 +0000

    [mod] to SO MRE

--- test.py
+++ test.py
@@ -1,66 +1,38 @@
-"""
-
-"""
 import os
-import logging
 
 from boto.dynamodb2 import connect_to_region
-from boto.dynamodb2.fields import HashKey, RangeKey
-from boto.dynamodb2.layer1 import DynamoDBConnection
+from boto.dynamodb2.fields import HashKey
 from boto.dynamodb2.table import Table
 import boto3
 
-logging.basicConfig(level="DEBUG")
+HOST = "localhost"
+PORT = "8000"
 
 os.environ["AWS_REGION"] = "us-east-1"
 os.environ["AWS_ACCESS_KEY_ID"] = "local"
 os.environ["AWS_SECRET_ACCESS_KEY"] = "local"
 
-HOST = "localhost"
-PORT = "8000"
 
-
-r = boto3.resource(
-    "dynamodb",
-    region_name=os.getenv("AWS_REGION"),
-    endpoint_url=f"http://{HOST}:{PORT}",
-)
-
-try:
-    table = r.create_table(
+def create_boto3_table(name):
+    r = boto3.resource(
+        "dynamodb", region_name=os.getenv("AWS_REGION"), endpoint_url=f"http://{HOST}:{PORT}"
+    )
+    r.create_table(
         AttributeDefinitions=[{"AttributeName": "string", "AttributeType": "S"}],
-        TableName="tableboto33",
+        TableName=name,
         KeySchema=[{"AttributeName": "string", "KeyType": "HASH"}],
         ProvisionedThroughput={"ReadCapacityUnits": 1, "WriteCapacityUnits": 1},
     )
-except Exception as e:
-    print(e)
-print(list(r.tables.all()))
-
-c = connect_to_region(
-    os.getenv("AWS_REGION"),
-    host=HOST,
-    port=PORT,
-    is_secure=False,
-)
+    print(list(r.tables.all()))
 
 
-try:
-    table2 = Table.create(
-        "tableboto222",
-        schema=[HashKey("string")],
-        throughput={"read": 1, "write": 1},
-        connection=c,
-    )
-except Exception as e:
-    print(e)
-print(c.list_tables())
+def create_boto2_table(name):
+    c = connect_to_region(os.getenv("AWS_REGION"), host=HOST, port=PORT, is_secure=False)
+    Table.create(name, schema=[HashKey("string")], throughput={"read": 1, "write": 1}, connection=c)
+    print(c.list_tables())
 
-c2 = DynamoDBConnection(
-    host=HOST,
-    port=PORT,
-    is_secure=False,
-)
 
-print(c2.list_tables())
-z = 1
+create_boto3_table("table-boto3-us-east-1")
+create_boto2_table("table-boto2-us-east-1")
+os.environ["AWS_REGION"] = "us-west-1"
+create_boto3_table("table-boto3-us-west-1")
